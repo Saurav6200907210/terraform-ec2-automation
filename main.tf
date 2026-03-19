02074d1a22df20a12edfdba2da9709f3c1b4ee14
Saurav Kumar <sonukumarteg245@gmail.com>
2026-03-19 10:26:25 +0530

    Comment out AWS provider and resource configurations

--- main.tf
+++ main.tf
@@ -12,26 +12,93 @@
 
 // create a ec2 instance using data source to fetch latest ami id and instance type
 # variables 
-provider "aws" {
-  region = var.aws_region
-}
+# provider "aws" {
+#   region = var.aws_region
+# }
 
-data "aws_ami" "amazon_linux" {
-  most_recent = true // ami ka letest version dega 
-  owners = ["amazon"] // amazon ke official ami ko filter karega
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true // ami ka letest version dega 
+#   owners = ["amazon"] // amazon ke official ami ko filter karega
 
-  filter {
-    name = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
+#   filter {
+#     name = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
 
-resource "aws_instance" "my_ec2" {
-  ami = data.aws_ami.amazon_linux.id
-  instance_type = var.instance_type
+# resource "aws_instance" "my_ec2" {
+#   ami = data.aws_ami.amazon_linux.id
+#   instance_type = var.instance_type
 
-  tags = {
-    Name = var.instance_name
-  }
+#   tags = {
+#     Name = var.instance_name
+#   }
   
-}
+# }
+
+//local values and conditional expression
+
+
+
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true // ami ka letest version dega 
+#   owners = ["amazon"] // amazon ke official ami ko filter karega
+
+#   filter {
+#     name = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
+# locals {
+#    name_tag = var.instance_type == "t3.micro" ? "T3 Micro Instance" : "standard instance"
+# }
+
+
+# resource "aws_instance" "my_ec2" {
+#   ami = data.aws_ami.amazon_linux.id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = local.name_tag
+#   }
+# }
+
+// Depends on** palhe s3 bucket create hoga fir ec2 iske liye use hota h depends on
+
+# provider "aws" {
+#   region = var.aws_region
+# }
+
+# data "aws_ami" "amazon_linux" {
+#   most_recent = true // ami ka letest version dega 
+#   owners = ["amazon"] // amazon ke official ami ko filter karega
+
+#   filter {
+#     name = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
+
+# resource "aws_s3_bucket" "my-bucket" {
+#   bucket = var.bucket_name
+
+#   tags = {
+#     Name = "Mys3Bucket"
+#   }
+# }
+
+# resource "aws_instance" "my_ec2" {
+#   ami = data.aws_ami.amazon_linux.id
+#   instance_type = var.instance_type
+
+#   tags = {
+#     Name = var.instance_name
+#   }
+#   depends_on = [
+#     aws_s3_bucket.my-bucket
+#   ]
+# }
